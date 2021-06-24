810631dcc4cd8d0022a6616335e7be373ec20880
Kamil Jasek <kamil.jasek@dbschenker.com>
2021-06-24 15:45:35 +0200

    prepare tests

--- src/main/java/pl/sda/refactoring/customers/CustomerService.java
+++ src/main/java/pl/sda/refactoring/customers/CustomerService.java
@@ -3,17 +3,26 @@ package pl.sda.refactoring.customers;
 import static java.util.Objects.requireNonNull;
 
 import java.time.LocalDateTime;
+import java.util.Properties;
 import java.util.UUID;
 import java.util.regex.Pattern;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public final class CustomerService {
 
     private final CustomerDao customerDao;
-    private final MailSender mailSender;
 
-    CustomerService(CustomerDao customerDao, MailSender mailSender) {
+    CustomerService(CustomerDao customerDao) {
         this.customerDao = requireNonNull(customerDao);
-        this.mailSender = requireNonNull(mailSender);
     }
 
     public boolean registerPerson(RegisterPersonForm form) {
@@ -39,7 +48,7 @@ public final class CustomerService {
                 "We registered you in our service. Please wait for verification!";
         }
         customerDao.save(customer);
-        mailSender.sendEmail(form.getEmail(), subj, body);
+        sendEmail(form.getEmail(), subj, body);
         return true;
     }
 
@@ -70,11 +79,11 @@ public final class CustomerService {
                 if (name.length() > 0 && name.matches("[\\p{L}\\s\\.]{2,100}")) {
                     customer.setCompName(name);
                 }
-                if (vat.length() == 10 && vat.matches("/\\d{10}/")) {
+                if (vat.length() == 10 && vat.matches("\\d{10}")) {
                     customer.setCompVat(vat);
                 }
 
-                if (customer.isValidPerson()) {
+                if (isValidCompany(customer)) {
                     result = true;
                 }
             }
@@ -100,10 +109,14 @@ public final class CustomerService {
             customer.setId(UUID.randomUUID());
             customerDao.save(customer);
             // send email to customer
-            mailSender.sendEmail(email, subj, body);
+            result = sendEmail(customer.getEmail(), subj, body);
         }
 
         return result;
+    }
+
+    boolean isValidCompany(Customer customer) {
+        return customer.getEmail() != null && customer.getCompName() != null && customer.getCompVat() != null;
     }
 
     /**
@@ -128,5 +141,43 @@ public final class CustomerService {
            result = true;
         }
         return result;
+    }
+
+    private boolean sendEmail(String address, String subj, String msg) {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", System.getenv().get("MAIL_SMTP_HOST"));
+        prop.put("mail.smtp.port", System.getenv().get("MAIL_SMTP_PORT"));
+        prop.put("mail.smtp.ssl.trust", System.getenv().get("MAIL_SMTP_SSL_TRUST"));
+
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("admin", "admin");
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("no-reply@company.com"));
+            message.setRecipients(
+                Message.RecipientType.TO, InternetAddress.parse(address));
+            message.setSubject(subj);
+
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(msg, "text/html");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+
+            message.setContent(multipart);
+
+            Transport.send(message);
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
     }
 }
